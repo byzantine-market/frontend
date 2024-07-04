2ad23724c7e6b6023e0f5175fdc4bc0cfde0608b
utxo detective <harrison@beckerich.org>
2024-07-04 13:55:29 -0400

    try

--- src/pages/api/subscribe.js
+++ src/pages/api/subscribe.js
@@ -4,16 +4,19 @@ import querystring from 'querystring'
 export default async function handler(req, res) {
 	const email = req.body.email
 
-	axios.post(
-		`${process.env.SENDY_URL}/subscribe`,
-		querystring.stringify({
-			list: process.env.SENDY_LIST,
-			email: email,
-			name: email,
-			api_key: process.env.SENDY_API_KEY
-		}),
-		{ headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' } }
-	)
-
-	res.status(200).json({ success: true })
+	try {
+		await axios.post(
+			`${process.env.SENDY_URL}/subscribe`,
+			querystring.stringify({
+				list: process.env.SENDY_LIST,
+				email: email,
+				name: email,
+				api_key: process.env.SENDY_API_KEY
+			}),
+			{ headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' } }
+		)
+		res.status(200).json({ success: true })
+	} catch (e) {
+		res.status(500).json({ error: e.message })
+	}
 }
